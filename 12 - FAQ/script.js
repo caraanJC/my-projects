6e5fea30911c38c083d1dd7c3ff202ec56c2937e
creation00 <caraanjohncarlo@gmail.com>
2021-04-05 23:59:12 +0800

    FAQ uploaded

---
+++ 12 - FAQ/script.js
@@ -0,0 +1,7 @@
+const faqs = document.querySelectorAll('.faq')
+const faq_toggles = document.querySelectorAll('.faq-toggle')
+faq_toggles.forEach((faq_toggle) => {
+  faq_toggle.addEventListener('click', () => {
+    faq_toggle.parentNode.classList.toggle('active')
+  })
+})
